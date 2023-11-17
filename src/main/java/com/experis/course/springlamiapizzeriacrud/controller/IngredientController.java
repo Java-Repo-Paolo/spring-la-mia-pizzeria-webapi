b2bec98c16e49441ae4e3cabe61936c2cfc2f276
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-17 17:01:40 +0100

    commit 6 - add list of ingredients

---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/controller/IngredientController.java
@@ -0,0 +1,30 @@
+package com.experis.course.springlamiapizzeriacrud.controller;
+
+import com.experis.course.springlamiapizzeriacrud.model.Ingredient;
+import com.experis.course.springlamiapizzeriacrud.model.Pizza;
+import com.experis.course.springlamiapizzeriacrud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping
+    public String index(Model model){
+        List<Ingredient> ingredientList;
+
+        ingredientList = ingredientRepository.findByOrderByName();
+        model.addAttribute("ingredientList", ingredientList);
+
+       return"ingredients/list";
+    }
+}
