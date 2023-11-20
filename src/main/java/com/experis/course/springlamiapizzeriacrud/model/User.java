de23615ed943e0e8b7f6d400987816ce1695adf9
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-20 14:57:49 +0100

    Commit 1 - add class User

---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/model/User.java
@@ -0,0 +1,66 @@
+package com.experis.course.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private LocalDateTime registerAt;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getRegisterAt() {
+        return registerAt;
+    }
+
+    public void setRegisterAt(LocalDateTime registerAt) {
+        this.registerAt = registerAt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
