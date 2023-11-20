962f6d5cb36e3cf40af6bc9ba43051d246c7ca61
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-20 15:54:55 +0100

    Commit 4 - add class UserDetails Service

---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/security/DatabaseUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.experis.course.springlamiapizzeriacrud.security;
+
+import com.experis.course.springlamiapizzeriacrud.model.User;
+import com.experis.course.springlamiapizzeriacrud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+       Optional<User> user = userRepository.findByEmail(username);
+
+       if(user.isPresent()) {
+           return new DatabaseUserDetails(user.get());
+       } else {
+           throw new UsernameNotFoundException("Username not found");
+       }
+    }
+}
