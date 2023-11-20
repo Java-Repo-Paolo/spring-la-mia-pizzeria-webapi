ee31ad91fff43805e5002b8988004e41ec2ab23f
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-20 15:56:53 +0100

    Commit 5 - add class Security Configuration

---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.experis.course.springlamiapizzeriacrud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfiguration {
+
+    // configurazione su come avere uno UserDetailsService
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    // configurazione su come avere un PasswordEncoder
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // configurazione dell'AuthenticationProvider
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return  authProvider;
+    }
+
+    // SecurityFilterChain che fa da "dogana"
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // le rotte /categories, /borrowings e /users solo per ADMIN
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/ingredients").hasAuthority("ADMIN")
+                .requestMatchers("/users").hasAuthority("ADMIN")
+                .requestMatchers("/offers/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/pizzas/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas", "/pizzas/**").hasAnyAuthority("ADMIN", "USER")
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout();
+        return http.build();
+    }
+
+}
