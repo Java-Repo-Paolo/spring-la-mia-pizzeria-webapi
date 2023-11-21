f7ce7a4c29506641d974313efe72800cda6dc3f9
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-21 17:58:02 +0100

    Commit - add Pagination

--- src/main/java/com/experis/course/springlamiapizzeriacrud/api/PizzaRestController.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -6,6 +6,10 @@ import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.service.PizzaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,12 +25,13 @@ public class PizzaRestController {
     private PizzaService pizzaService;
 
 
-    /*http://localhost:8080/api/v1/pizzas*/
+    //http://localhost:8080/api/v1/pizzas
     @GetMapping
     public List<Pizza> index (@RequestParam Optional<String> search){
         return pizzaService.getPizzaList(search);
     }
 
+    //http://localhost:8080/api/v1/pizzas/id
     @GetMapping("/{id}")
     public Pizza details(@PathVariable Integer id) {
         try {
@@ -36,6 +41,7 @@ public class PizzaRestController {
         }
     }
 
+    //http://localhost:8080/api/v1/pizzas
     @PostMapping
     public Pizza create(@Valid @RequestBody Pizza book) {
         try {
@@ -45,6 +51,7 @@ public class PizzaRestController {
         }
     }
 
+    //http://localhost:8080/api/v1/pizzas/id
     @PutMapping("/{id}")
     public Pizza update(@PathVariable Integer id, @Valid @RequestBody Pizza book) {
         book.setId(id);
@@ -55,6 +62,7 @@ public class PizzaRestController {
         }
     }
 
+    //http://localhost:8080/api/v1/pizzas/id
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
         try {
@@ -63,5 +71,20 @@ public class PizzaRestController {
         } catch (PizzaNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    //Paginazione
+    @GetMapping("/page")
+    public Page<Pizza> pagedIndex(
+            @RequestParam(name = "size", defaultValue = "5") Integer size,
+            @RequestParam(name = "page", defaultValue = "0") Integer page) {
+
+        return pizzaService.getPage(PageRequest.of(page, size));
+    }
+
+    //http://localhost:8080/api/v1/pizzas/page/v2
+    @GetMapping("/page/v2")
+    public Page<Pizza> pagedIndexV2(@PageableDefault(page = 0, size = 5) Pageable pageable) {
+        return pizzaService.getPage(pageable);
     }
 }
--- src/main/java/com/experis/course/springlamiapizzeriacrud/service/PizzaService.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/service/PizzaService.java
@@ -5,6 +5,8 @@ import com.experis.course.springlamiapizzeriacrud.exceptions.PizzaNotFoundExcept
 import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,6 +19,7 @@ public class PizzaService {
     @Autowired
     private PizzaRepository pizzaRepository;
 
+    //Index
     public List<Pizza> getPizzaList(Optional<String> search){
         if (search.isPresent()){
             return pizzaRepository.findByNameContainingIgnoreCase(search.get());
@@ -24,7 +27,11 @@ public class PizzaService {
             return pizzaRepository.findAll();
         }
     }
+    public List<Pizza> getPizzaList() {
+        return pizzaRepository.findAll();
+    }
 
+    //Show
     public Pizza getPizzaById(Integer id) throws PizzaNotFoundException{
         Optional<Pizza> result = pizzaRepository.findById(id);
         if (result.isPresent()){
@@ -34,6 +41,7 @@ public class PizzaService {
         }
     }
 
+    //Create
     public Pizza createPizza(Pizza pizza) throws PizzaNameUniqueException {
         pizza.setId(null);
         try{
@@ -43,6 +51,7 @@ public class PizzaService {
         }
     }
 
+    //Edit
     public Pizza editPizza(Pizza pizza) throws PizzaNotFoundException {
         Pizza pizzaToEdit = getPizzaById(pizza.getId());
 
@@ -55,7 +64,13 @@ public class PizzaService {
         return pizzaRepository.save(pizzaToEdit);
     }
 
+    //Delete
     public void deletePizza (Integer id){
         pizzaRepository.deleteById(id);
+    }
+
+    //Paginazione
+    public Page<Pizza> getPage(Pageable pageable) {
+        return pizzaRepository.findAll(pageable);
     }
 }
