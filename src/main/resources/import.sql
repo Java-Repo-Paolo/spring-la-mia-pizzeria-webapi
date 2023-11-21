0616c0d344414dd7d0be7c7d8ec653079975c230
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-21 17:31:33 +0100

    Commit - add Ingredients in Pizzas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,23 +1,23 @@
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:50:00', 'Una deliziosa pizza con mozzarella, pomodoro e basilico.', 'https://primochef.it/wp-content/uploads/2019/08/SH_pizza_fatta_in_casa-1200x800.jpg', 'Pizza Margherita', 8.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:51:00', 'Una pizza classica con pepperoni e formaggio fuso.', 'https://cdn.tasteatlas.com/images/dishes/b05a0af72ad845f3a6abe16143d7853a.jpg?m=facebook', 'Pizza Pepperoni', 9.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:52:00', 'Una pizza con prosciutto, funghi, carciofi e olive.', 'https://www.petitchef.it/imgupl/recipe/pizza-quattro-stagioni--327908p533338.jpg', 'Pizza Quattro Stagioni', 10.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:53:00', 'Pizza con prosciutto, funghi e una base di pomodoro.', 'https://www.pizzeriasenzatempo.it/wp-content/uploads/2020/04/PROSCIUTTO-E-FUNGHI.jpg', 'Pizza Prosciutto Funghi', 9.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:54:00', 'Una pizza piccante con salame piccante e peperoncini.', 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/03/d1.jpg', 'Pizza Diavola', 10.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:55:00', 'Una pizza tropicale con prosciutto e ananas.', 'https://www.fruttaweb.com/consigli/wp-content/uploads/2019/07/shutterstock_84904861.jpg', 'Pizza Hawaii', 11.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:56:00', 'Pizza con prosciutto, carciofi, olive e funghi.', 'https://www.melarossa.it/wp-content/uploads/2021/02/pizza-capricciosa.jpg', 'Pizza Capricciosa', 11.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:57:00', 'Una pizza semplice con aglio, pomodoro e basilico.', 'https://www.giallozafferano.it/images/154-15407/Pizza-marinara_650x433_wm.jpg', 'Pizza Marinara', 8.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:58:00', 'Pizza con una selezione di quattro formaggi.', 'https://primochef.it/wp-content/uploads/2020/04/SH_pizza_quattro_stagioni.jpg', 'Pizza Quattro Formaggi', 12.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 14:59:00', 'Pizza con tonno, cipolla, olive e pomodoro.', 'https://blog.giallozafferano.it/cucinoperpassione/wp-content/uploads/2019/09/Pizza-rossa-tonno-e-cipolla.jpg', 'Pizza Tonno e Cipolla', 10.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:00:00', 'Una pizza napoletana con acciughe e capperi.', 'https://www.donnamoderna.com/content/uploads/2021/08/pizza-napoli-830x625.jpg', 'Pizza Napoletana', 9.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:01:00', 'Una variante della classica Margherita con mozzarella di bufala.', 'https://www.mypizzamoments.com/wp-content/uploads/2017/03/MyPizza-Margherita-extra-1024x683.jpeg', 'Pizza Margherita Extra', 11.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:02:00', 'Una deliziosa pizza vegetariana con verdure fresche.', 'https://www.guardini.com/images/guardinispa/ricette/full/pizza_set_2021_full.jpg', 'Pizza Vegetariana', 10.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:03:00', 'Pizza con ricotta, mozzarella, parmigiano e pepe nero.', 'https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/03/Pizza-mimosa-1200x675.jpg', 'Pizza Bianca', 9.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:04:00', 'Una pizza cremosa con pancetta, uova e formaggio.', 'https://www.pizzatales.it/wp50/wp-content/uploads/2021/10/la-pizza-alla-carbonara.jpg', 'Pizza Carbonara', 12.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:05:00', 'Una pizza con rucola fresca e prosciutto crudo.', 'https://blog.giallozafferano.it/primipiattiricette/wp-content/uploads/2015/10/gluten-free-pizza.jpg', 'Pizza Rucola e Prosciutto', 12.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:06:00', 'Una pizza piegata a calzone ripiena di prosciutto e formaggio.', 'https://img.comeunochef.com/2020-11/calzone-napoletano-1.jpg', 'Pizza Calzone', 11.49);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:07:00', 'Pizza con una selezione mista di frutti di mare.', 'https://wips.plug.it/cips/buonissimo.org/cms/2012/05/pizza-ai-frutti-di-mare.jpg', 'Pizza Frutti di Mare', 13.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:08:00', 'Una pizza ispirata ai sapori della Sicilia.', 'https://www.strapizza.it/media/k2/items/cache/af2ef6a0e2c9c528b09655df79f3b312_XL.jpg', 'Pizza Siciliana', 11.99);
-INSERT INTO pizze (created_at, description, image, name, price) VALUES ('2023-11-09 15:09:00', 'Pizza con funghi, mozzarella e prelibate erbe aromatiche.', 'https://ohmydish.com/wp-content/uploads/2017/07/Pizza-funghi.jpg', 'Pizza Funghi', 9.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:50:00', 'Una deliziosa pizza con mozzarella, pomodoro e basilico.', 'https://primochef.it/wp-content/uploads/2019/08/SH_pizza_fatta_in_casa-1200x800.jpg', 'Pizza Margherita', 8.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:51:00', 'Una pizza classica con pepperoni e formaggio fuso.', 'https://cdn.tasteatlas.com/images/dishes/b05a0af72ad845f3a6abe16143d7853a.jpg?m=facebook', 'Pizza Pepperoni', 9.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:52:00', 'Una pizza con prosciutto, funghi, carciofi e olive.', 'https://www.petitchef.it/imgupl/recipe/pizza-quattro-stagioni--327908p533338.jpg', 'Pizza Quattro Stagioni', 10.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:53:00', 'Pizza con prosciutto, funghi e una base di pomodoro.', 'https://www.pizzeriasenzatempo.it/wp-content/uploads/2020/04/PROSCIUTTO-E-FUNGHI.jpg', 'Pizza Prosciutto Funghi', 9.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:54:00', 'Una pizza piccante con salame piccante e peperoncini.', 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/03/d1.jpg', 'Pizza Diavola', 10.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:55:00', 'Una pizza tropicale con prosciutto e ananas.', 'https://www.fruttaweb.com/consigli/wp-content/uploads/2019/07/shutterstock_84904861.jpg', 'Pizza Hawaii', 11.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:56:00', 'Pizza con prosciutto, carciofi, olive e funghi.', 'https://www.melarossa.it/wp-content/uploads/2021/02/pizza-capricciosa.jpg', 'Pizza Capricciosa', 11.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:57:00', 'Una pizza semplice con aglio, pomodoro e basilico.', 'https://www.giallozafferano.it/images/154-15407/Pizza-marinara_650x433_wm.jpg', 'Pizza Marinara', 8.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:58:00', 'Pizza con una selezione di quattro formaggi.', 'https://primochef.it/wp-content/uploads/2020/04/SH_pizza_quattro_stagioni.jpg', 'Pizza Quattro Formaggi', 12.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 14:59:00', 'Pizza con tonno, cipolla, olive e pomodoro.', 'https://blog.giallozafferano.it/cucinoperpassione/wp-content/uploads/2019/09/Pizza-rossa-tonno-e-cipolla.jpg', 'Pizza Tonno e Cipolla', 10.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:00:00', 'Una pizza napoletana con acciughe e capperi.', 'https://www.donnamoderna.com/content/uploads/2021/08/pizza-napoli-830x625.jpg', 'Pizza Napoletana', 9.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:01:00', 'Una variante della classica Margherita con mozzarella di bufala.', 'https://www.mypizzamoments.com/wp-content/uploads/2017/03/MyPizza-Margherita-extra-1024x683.jpeg', 'Pizza Margherita Extra', 11.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:02:00', 'Una deliziosa pizza vegetariana con verdure fresche.', 'https://www.guardini.com/images/guardinispa/ricette/full/pizza_set_2021_full.jpg', 'Pizza Vegetariana', 10.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:03:00', 'Pizza con ricotta, mozzarella, parmigiano e pepe nero.', 'https://staticcookist.akamaized.net/wp-content/uploads/sites/21/2023/03/Pizza-mimosa-1200x675.jpg', 'Pizza Bianca', 9.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:04:00', 'Una pizza cremosa con pancetta, uova e formaggio.', 'https://www.pizzatales.it/wp50/wp-content/uploads/2021/10/la-pizza-alla-carbonara.jpg', 'Pizza Carbonara', 12.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:05:00', 'Una pizza con rucola fresca e prosciutto crudo.', 'https://blog.giallozafferano.it/primipiattiricette/wp-content/uploads/2015/10/gluten-free-pizza.jpg', 'Pizza Rucola e Prosciutto', 12.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:06:00', 'Una pizza piegata a calzone ripiena di prosciutto e formaggio.', 'https://img.comeunochef.com/2020-11/calzone-napoletano-1.jpg', 'Pizza Calzone', 11.49);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:07:00', 'Pizza con una selezione mista di frutti di mare.', 'https://wips.plug.it/cips/buonissimo.org/cms/2012/05/pizza-ai-frutti-di-mare.jpg', 'Pizza Frutti di Mare', 13.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:08:00', 'Una pizza ispirata ai sapori della Sicilia.', 'https://www.strapizza.it/media/k2/items/cache/af2ef6a0e2c9c528b09655df79f3b312_XL.jpg', 'Pizza Siciliana', 11.99);
+INSERT INTO pizzas (created_at, description, image, name, price) VALUES ('2023-11-09 15:09:00', 'Pizza con funghi, mozzarella e prelibate erbe aromatiche.', 'https://ohmydish.com/wp-content/uploads/2017/07/Pizza-funghi.jpg', 'Pizza Funghi', 9.49);
 
 INSERT INTO ingredients (name) VALUES ('Mozzarella');
 INSERT INTO ingredients (name) VALUES ('Pomodoro');
@@ -59,6 +59,76 @@ INSERT INTO ingredients (name) VALUES ('Cetriolini');
 INSERT INTO ingredients (name) VALUES ('Pesto Rosso');
 INSERT INTO ingredients (name) VALUES ('Asparagi');
 INSERT INTO ingredients (name) VALUES ('Fungi Porcini');
+
+-- Pizza Margherita con Mozzarella e Pomodoro
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES (1,1);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES (1,2);
+-- Pizza Pepperoni con Prosciutto e Funghi
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(2,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(2,6);
+-- Pizza Quattro Stagioni con Prosciutto, Funghi, Carciofi, e Olive
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(3,3);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(3,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(3,6);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(3,7);
+-- Pizza Prosciutto Funghi con Prosciutto e Funghi
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(4,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(4,6);
+-- Pizza Diavola con Salame Piccante e Peperoncini
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(5,20);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(5,33);
+-- Pizza Hawaii con Prosciutto e Ananas
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(6,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(6,9);
+-- Pizza Capricciosa con Prosciutto, Carciofi, Olive, e Funghi
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(7,3);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(7,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(7,6);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(7,7);
+-- Pizza Marinara con Aglio, Pomodoro, e Basilico
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(8,2);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(8,19);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(8,30);
+-- Pizza Tonno e Cipolla con Tonno, Cipolla, Olive, e Pomodoro
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(10,2);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(10,7);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(10,10);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(10,14);
+-- Pizza Napoletana con Acciughe e Capperi
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(11,12);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(11,13);
+-- Pizza Margherita Extra con Mozzarella di Bufala
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(12,1);
+-- Pizza Vegetariana con Verdure Fresche
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(13,3);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(13,7);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(13,15);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(13,32);
+-- Pizza Bianca con Ricotta, Mozzarella, Parmigiano, e Pepe Nero
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(14,1);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(14,18);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(14,26);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(14,34);
+-- Pizza Carbonara con Pancetta, Uova, e Formaggio
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(15,8);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(15,18);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(15,24);
+-- Pizza Rucola e Prosciutto con Rucola Fresca e Prosciutto Crudo
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(16,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(16,13);
+-- Pizza Calzone con Piegatura a Calzone Ripiena di Prosciutto e Formaggio
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(17,5);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(17,6);
+-- Pizza Frutti di Mare con Selezione Mista di Frutti di Mare
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(18,25);
+-- Pizza Siciliana con Ispirazione ai Sapori della Sicilia
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(19,6);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(19,8);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(19,14);
+-- Pizza Funghi con Funghi, Mozzarella, e Prelibate Erbe Aromatiche
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(20,1);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(20,3);
+INSERT INTO pizzas_ingredients(pizza_id, ingredients_id) VALUES(20,35);
 
 INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
 INSERT INTO roles (id, name) VALUES(2, 'USER');
